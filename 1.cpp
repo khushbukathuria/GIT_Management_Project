6a3fc26c4b3dc43df5c54af47c6a5e08bc82e63a
prashasti0312 <500068493@stu.upes.ac.in>
2019-04-23 18:05:27 +0530

    first commit by prashasti pushing travel detail function

--- 1.cpp
+++ 1.cpp
@@ -100,4 +100,97 @@ cout<<”\t”<<name[i]<<”\t\t”<<age[i]<<”\t\t”<<sex[i]<<”\t\t”<<pas
 getch();
 }
 
-
+class travdetails   //class which holds the travel details
+{
+int trvtcode;
+int day;
+int month;
+int year;
+int num1;
+int bdg_pt;
+int go_pt;
+int deck;
+int cabin;
+int pool;
+int gym;
+int sports;
+int salon;
+int spa;
+int theatre;
+public:
+void initial();
+void t_input(int);
+void t_output();
+void compute();
+int gtcode();
+void accept(int);
+}tob;
+void travdetails::initial()
+{
+pool=gym=theatre=sports=salon=0;
+}
+void travdetails::accept(int c)
+{
+num1=c;
+}
+int travdetails::gtcode()
+{
+return trvtcode;
+}
+void travdetails::t_input(int cd) //input func()-class2
+{
+tob.initial();
+trvtcode=cd;
+int i=1,opt;
+clrscr();
+do
+{
+clrscr();
+cout<<endl;
+cout<<”\n\n\t\t\t\t_||__TRAVEL DETAILS__”;
+cout<<”\n\t\t\t\t\\___________________/”;
+cout<<”\n\t\t\t\t~~~~~~~~~~~~~~~~~~~~~~”;
+cout<<”\n\n\t\tPlease enter the following details for your journey:”;
+cout<<”\n\n\t\t*Note:Options marked with ‘*’ are compulsory.\n\t\t\tPlease do select them.”;
+cout<<”\n\n\n\t\t\t\t*1.Boarding Point\n\t\t\t\t*2.Destination\n\t\t\t\t*3.Date of Departure\n\t\t\t\t*4.Deck\n\t\t\t\t 5.Cabin\n\t\t\t\t 6.Swimming Pool\n\t\t\t\t 7.Gymnasuim\n\t\t\t\t 8.Sports\n\t\t\t\t 9.Salon\n\t\t\t\t 10.Spa\n\t\t\t\t 11.Theatre\n\t\t\t\t 12.Back\n\n\t\t\t\t “;
+cin>>opt;
+switch(opt)
+{
+case 1:clrscr();
+cout<<”\n\n\n\t\t\tSelect Boarding point:\n\n\t\t\t1.Mumbai\n\n\t\t\t2.Cochin\n\n\t\t\t3.Chennai\n\n\t\t\t “;
+cin>>bdg_pt;
+break;
+case 2:clrscr();
+cout<<”\n\n\t\t\t****** Select Destination ******\n\n\n”;
+cout<<”\t 1.New York\t\t6.Dubai\t\t\t11.Antananariv”;
+cout<<”\n\n\t 2.Miami\t\t7.Lisbon\t\t12.Cairo\n\n”;
+cout<<”\t 3.Rio De Janeiro\t8.London\t\t13.Perth”;
+cout<<”\n\n\t 4.Colombo\t\t9.Copenhagen\t\t14.Sydney”;
+cout<<”\n\n\t 5.Hong Kong\t\t10.Cape Town\t\t15.Wellington\n\n\n\n\n\t\t\t\t “;
+cin>>go_pt;
+break;
+case 3:clrscr();
+cout<<”\n\n\t Date of Departure ”;
+cout<<”\n\n   * Note:Format for entering:Day(press enter)Month(press enter)Year *”;
+cout<<”\n\n\t\tEnter your preferred date of departure: “;
+cin>>day;
+gotoxy(59,7);   //to create a paricular format for entry
+cout<<”/”;
+cin>>month;
+gotoxy(62,7);
+cout<<”/”;
+cin>>year;
+break;
+case 4:clrscr();
+cout<<”\n\n\t\t\tEnter The Choice OF Deck\n\n\t\t\t1.Top Deck\n\n\t\t\t2.Middle Decks\n\n\t\t\t3.Bottom Deck\n\n\t\t\t “;
+cin>>deck;
+break;
+case 5:clrscr();
+cout<<”\n\n\t\t\tEnter The Choice Of Cabin\n\n\t\t\t1.AC\n\n\t\t\t2.Non AC\n\n\t\t\t “;
+cin>>cabin;
+break;
+case 6:clrscr();
+cout<<”\n\n\t\tDo You Like To Avail The Facility Of A Swimming Pool\n\n\t\t\t1.No\n\n\t\t\t2.Yes\n\n\t\t\t “;
+cin>>pool;
+pool–;
+break;
