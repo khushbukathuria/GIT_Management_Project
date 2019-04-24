bdf75c0b682adff63b91081129681f4a52b6342e
prashasti0312 <500068493@stu.upes.ac.in>
2019-04-24 23:23:59 +0530

    commit by prashasti

--- 1.cpp
+++ 1.cpp
@@ -100,4 +100,119 @@ cout<<”\t”<<name[i]<<”\t\t”<<age[i]<<”\t\t”<<sex[i]<<”\t\t”<<pas
 getch();
 }
 
-
+void deletion(int c)  //common delete func()
+{
+ofstream ofl2(“temp1.txt”,ios::binary);
+if(!ofl2)
+cout<<”Error While Opening File”;
+ifstream ifl4(“PersonalDetails.txt”,ios::binary);
+if(!ifl4)
+cout<<”Error While Opening File”;
+ifl4.read((char*)&pob,sizeof(pob));
+while(!ifl4.eof())
+{
+if(pob.givecode()!=c)
+{
+ofl2.write((char*)&pob,sizeof(pob));
+}
+ifl4.read((char*)&pob,sizeof(pob));
+}
+remove(“PersonalDetails.txt”);
+rename(“temp1.txt”,”PersonalDetails.txt”);
+ofl2.close();
+ifl4.close();
+ofstream ofl3(“temp2.txt”,ios::binary);
+if(!ofl3)
+cout<<”\nError While Opening File”;
+ifstream ifl5(“TravelDetails.txt”,ios::binary);
+if(!ifl5)
+cout<<”\nError While Opening File”;
+ifl5.read((char*)&tob,sizeof(tob));
+while(!ifl5.eof())
+{
+if(tob.gtcode()!=c)
+{
+ofl3.write((char*)&tob,sizeof(tob));
+}
+ifl5.read((char*)&tob,sizeof(tob));
+}
+ofl3.close();
+ifl5.close();
+remove(“TravelDetails.txt”);
+rename(“temp1.txt”,”TravelDetails.txt”);
+cout<<”\n\n\t\tDeletion Completed!”;
+getch();
+}
+void travdetails::compute()   //compution+bill generation
+{
+long int gttl=0,hr,dcst,dck,cabn,swpool=5000,gm=2000,spfts=7500,slon=6000,sp=20000,ttr=500;
+switch(go_pt)
+{
+case 1:;
+case 2:;
+case 3:hr=30*24;
+dcst=250000;
+break;
+case 4:;
+case 5:;
+case 6:hr=7*24;
+dcst=75000;
+break;
+case 7:;
+case 8:;
+case 9:hr=24*24;
+dcst=130000;
+break;
+case 10:;
+case 11:;
+case 12:hr=15*24;
+dcst=100000;
+break;
+case 13:;
+case 14:;
+case 15:hr=12*24;
+dcst=120000;
+break;
+}
+switch(deck)
+{
+case 1:dck=7500;
+break;
+case 2:dck=5000;
+break;
+case 3:dck=1000;
+}
+switch(cabin)
+{
+case 1:cabn=5000;
+break;
+case 2:cabn=2000;
+break;
+}
+clrscr();
+cout<<”\n\n\t\t:::::::::::::::::::::: BILL ::::::::::::::::::::::::::”;
+cout<<”\n\n\t\tBoarding point:\t\t “;
+boardpt(bdg_pt);
+cout<<”\n\n\t\tDestination:\t\t “;
+dest(go_pt);
+cout<<”\n\n\t\tDate of Departure: “;
+cout<<day<<”/”<<month<<”/”<<year;
+hr=hr/24;         //to calculate date of arrival
+day=day+hr;
+if(month==1||month==3||month==5||month==7||month==8||month==10||month==12)
+{
+if(day>31)
+{
+month=month+1;
+day=day%31;
+}
+}
+if(month==4||month==6||month==9||month==11)
+{
+if(day>30)
+{
+month=month+1;
+day=day%30;
+}
+}
+if(month==2)
